6f36fbf5205b820fde2e4f9c9b8be5e3ec5551de
Om Tayade <omtayade160901@gmail.com>
2021-10-09 00:30:00 +0530

    Done Css changes for Dashboard and nav

--- client/src/Components/Navbar/Burger/burger.jsx
+++ client/src/Components/Navbar/Burger/burger.jsx
@@ -18,7 +18,7 @@ const StyledBurger = styled.div`
   div {
     width: 2rem;
     height: 0.25rem;
-    background-color: ${({ open }) => open ? '#ccc' : '#9d0208'};
+    background-color: ${({ open }) => open ? '#ccc' : '#fff'};
     border-radius: 10px;
     transform-origin: 1px;
     transition: all 0.3s linear;
