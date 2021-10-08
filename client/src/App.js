936b07fb23a6f9e4b255303ba8b0489fe1ad456e
gayatri2 <gayatri.godbole02@gmail.com>
2021-10-08 21:28:04 +0530

    Menu page completed.

--- client/src/App.js
+++ client/src/App.js
@@ -36,7 +36,7 @@ function App() {
           
           <Route exact path="/menu">
             <NavBar/>
-            {/* <Menu/> */}
+            <Menu/>
           </Route>
           <Route exact path="/inventory">
             <NavBar/>
